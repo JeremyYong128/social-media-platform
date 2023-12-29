64156ca722e876bf3bf97b60aabcb0c951cc3927
Jeremy <jeremyyong0101@gmail.com>
2023-12-29 14:08:42 +0800

    Configure CORS origin to wildcard

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express()
 // middleware
 app.use(express.json())
 app.use(cors({
-    origin: 'https://658e51f4c44d5b00077f9c99--weichat.netlify.app'
+    origin: '*'
 }))
 app.use((req, routes, next) => {
     console.log(req.path, req.method)
