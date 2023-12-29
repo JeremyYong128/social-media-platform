9971fce07ec767ec473388945f848bf305c9bfdd
Jeremy <jeremyyong0101@gmail.com>
2023-12-29 12:59:51 +0800

    Update cors origin url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express()
 // middleware
 app.use(express.json())
 app.use(cors({
-    origin: 'https://658e48e70ce0fb69dcbd9a7d--weichat.netlify.app'
+    origin: 'https://658e51f4c44d5b00077f9c99--weichat.netlify.app'
 }))
 app.use((req, routes, next) => {
     console.log(req.path, req.method)
